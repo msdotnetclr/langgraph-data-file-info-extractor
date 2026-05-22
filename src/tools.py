2c427f4059aaa7899e07c402cfbf39a74ad50af2
Miracle Zhou <msdotnetclr@gmail.com>
2026-05-22 07:51:20 -0400

    refactor: remove get_weather tool from tools list

--- src/tools.py
+++ src/tools.py
@@ -1,17 +1,4 @@
 from langchain_core.tools import tool
 
-@tool
-def get_weather(location: str) -> str:
-    """Get the current weather for a given location. Use this to find out weather info."""
-    loc = location.lower()
-    if "london" in loc:
-        return "It's 15°C and raining in London."
-    elif "new york" in loc:
-        return "It's 22°C and sunny in New York."
-    elif "tokyo" in loc:
-        return "It's 18°C and cloudy in Tokyo."
-    else:
-        return f"The weather in {location} is 20°C and partly cloudy."
 
-# List of tools to export
-tools = [get_weather]
+tools = []
